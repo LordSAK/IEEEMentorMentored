c874ae74330a3eb1e0ce4c858a0ab52ccb26b76b
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-08-26 17:09:43 +0500

    removed to_s

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -14,9 +14,9 @@ class ProfileController < ApplicationController
   	else
   		@user_id= params[:id]
       @user=User.find(@user_id)
-      if !@user.blank?
-        @user_private = @user.Private.to_s
-      end
+
+        @user_private = @user.Private
+
       @user_education = Education.where('"UserID" = ? and "Private" = ?', params[:id],"1")
       @user_communication = Communication.where('"UserID" = ? and "Private" = ?',params[:id],"1")
       @user_profession = Profession.where('"UserID" =  ? and "Private" = ?',params[:id],"1")
