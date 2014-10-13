b63c30e8d223e666f3c223ea0a9a73ef839a767c
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-10-13 13:01:15 +0500

    added using for change column

--- db/migrate/20141010102613_change_university_id_datatype.rb
+++ db/migrate/20141010102613_change_university_id_datatype.rb
@@ -1,7 +1,7 @@
 class ChangeUniversityIdDatatype < ActiveRecord::Migration
 	def self.up
 		change_table :educations do |t|
-			t.change :UniversityID, :integer
+			t.change :UniversityID, :integer , 'integer  USING CAST("RequestFrom" AS integer)'
 		end
 	end
 	
