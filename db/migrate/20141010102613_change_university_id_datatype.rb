331ea579ce9fb11d5ebdc8d4af827badb510fc97
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-10-13 13:23:10 +0500

    changed using syntax in migration for datatype change

--- db/migrate/20141010102613_change_university_id_datatype.rb
+++ db/migrate/20141010102613_change_university_id_datatype.rb
@@ -1,8 +1,6 @@
 class ChangeUniversityIdDatatype < ActiveRecord::Migration
 	def self.up
-		change_table :educations do |t|
-			t.change :UniversityID, :integer , 'integer  USING CAST("UniversityID" AS integer)'
-		end
+		change_column :educations, :UniversityID, 'integer USING CAST(UniversityID AS integer)'
 	end
 	
   	def self.down
